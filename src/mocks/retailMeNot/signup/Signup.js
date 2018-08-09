2a48c4c82c5af6d1d85b2a69ae266b998fd4a80c
Ryan Margono <rmargono@paypal.com>
2018-08-08 17:23:38 -0700

    more padding

--- src/mocks/retailMeNot/signup/Signup.js
+++ src/mocks/retailMeNot/signup/Signup.js
@@ -11,22 +11,22 @@ import SignupPP from './SignupPP';
 const Signup = () => (
     <Paper>
         <Grid container spacing={0}>
-            <Grid item xs={12} style={{ paddingLeft: '1%', paddingTop: '5%' }}>
+            <Grid item xs={12} style={{ paddingLeft: '2%', paddingTop: '5%' }}>
                 <SignupPP />
             </Grid>
-            <Grid item xs={12} style={{ padding: '5%', paddingTop: '5%', paddingBottom: '5%' }}>
+            <Grid item xs={12} style={{ padding: '2%', paddingTop: '2%', paddingBottom: '5%' }}>
                 <Button fullWidth style={{ padding: '5%' }} variant='raised' color='primary'> Continue with Facebook </Button>
             </Grid>
             <Grid item xs={12}>
                 <Divider />
             </Grid>
-            <Grid item xs={12} style={{ padding: '5%' }}>
+            <Grid item xs={12} style={{ padding: '2%' }}>
                 <TextField label="Username" fullWidth />
             </Grid>
-            <Grid item xs={12} style={{ padding: '5%' }}>
+            <Grid item xs={12} style={{ padding: '2%' }}>
                 <TextField label="Password" fullWidth />
             </Grid>
-            <Grid item xs={12} style={{ padding: '5%' }}>
+            <Grid item xs={12} style={{ padding: '2%' }}>
                 <Button fullWidth style={{padding: '5%'}} variant='raised' color='secondary'> Sign Up </Button>
             </Grid>
         </Grid>
