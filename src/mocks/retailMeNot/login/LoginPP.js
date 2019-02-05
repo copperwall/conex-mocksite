2ceeec0f9d39e4bcb85343289af51d97ddfded2e
Chris Opperwall <copperwall@paypal.com>
2019-02-05 11:30:06 -0800

    Use new SDK

--- src/mocks/retailMeNot/login/LoginPP.js
+++ src/mocks/retailMeNot/login/LoginPP.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 
 class LoginPP extends React.Component {
     render() {
-        const RmnReactZoid = window.AdvConnectZoid.driver('react', {
+        const PayoutsConnect = window.paypal.PayoutsConnect.driver('react', {
             React,
             ReactDOM
         });
         return (
-            <RmnReactZoid
-                clientId="AUfKX2mdsxU8RmnSRUvz5PaLGS7hDeLe1jmgORBV2Ar4mJjvaczl-TNZaazvcsqnkIhk69IYia3PBQsH"
-                deviceType="mobile"
+            <PayoutsConnect
+                clientId={{
+                    production: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf",
+                    sandbox: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf"
+                }}
+                env="sandbox"
                 pageType="login"
                 merchantId="abc123"
                 onLogin={() => window.location.href = `${window.location.origin}/retailmenot/payout`}
