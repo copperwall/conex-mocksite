6a52e5bda8b8a5945a9b1cd65001529e8340b7f9
Chris Opperwall <copperwall@paypal.com>
2019-02-13 13:34:49 -0800

    Update signup page to use new SDK

--- src/mocks/retailMeNot/signup/SignupPP.js
+++ src/mocks/retailMeNot/signup/SignupPP.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 
 class SignupPP extends React.Component {
     render() {
-        let RmnReactZoid = window.AdvConnectZoid.driver('react', {
+        const PayoutsConnect = window.paypal.PayoutsConnect.driver('react', {
             React,
             ReactDOM
         });
         return (
-            <RmnReactZoid
-                clientId="AXgFMu3u6vdatxW4Yfl9R5A8QBa_Wm-H4ijwUnOFruS60g55Jqaj1L91LDyneyIuScZpMqLo2ciBcvd3"
-                deviceType="mobile"
+            <PayoutsConnect
+                clientId={{
+                    production: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf",
+                    sandbox: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf"
+                }}
+                env="sandbox"
                 pageType="signup"
+                merchantId="abc123"
                 onLogin={() => window.location.href = `${window.location.origin}/retailmenot/payout`}
             />
         )
