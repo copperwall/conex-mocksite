6de5a9c3ef48d18db57b3392d7ab042ba61e7edd
Chris Opperwall <copperwall@paypal.com>
2019-02-21 11:14:55 -0800

    stage stuff

--- src/mocks/retailMeNot/signup/SignupPP.js
+++ src/mocks/retailMeNot/signup/SignupPP.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 
 class SignupPP extends React.Component {
     render() {
-        const PayoutsConnect = window.paypal.PayoutsConnect.driver('react', {
+        const PayoutsAAC = window.paypal.PayoutsAAC.driver('react', {
             React,
             ReactDOM
         });
         return (
-            <PayoutsConnect
+            <PayoutsAAC
                 clientId={{
                     production: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf",
-                    sandbox: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf"
+                    sandbox: "AbGEld_rV8YPIC1pMieTvZqrXfx8vMLuit71kDwCEy7ypeAJMc29--Asg2vjXh_fXz8HWeY_znOGwDPf",
+                    staging: "AUfKX2mdsxU8RmnSRUvz5PaLGS7hDeLe1jmgORBV2Ar4mJjvaczl-TNZaazvcsqnkIhk69IYia3PBQsH"
                 }}
-                env="sandbox"
+                env="staging"
                 pageType="signup"
                 merchantId="abc123"
                 onLogin={() => window.location.href = `${window.location.origin}/retailmenot/payout`}
